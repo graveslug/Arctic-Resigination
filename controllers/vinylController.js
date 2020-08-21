7782c9e4a32e6955fd68ac1c13035b3b1c5623f5
Philip Gray <pcgrayiii@icloud.com>
2020-08-21 11:50:31 -0700

    changed controllers/vinyljs into controllers/vinylControllerjs. Transferred the controllers from server.js to VinylControllers.js for future dyanmic implementation and modular control. Added new route to server.js to connect to vinylController

---
+++ controllers/vinylController.js
@@ -0,0 +1,90 @@
+//==================
+//  Routes        //
+//==================
+
+
+app.get('/', (req, res) => {
+    res.redirect('/arcticresigination')
+})
+
+// INDEX
+app.get('/arcticresigination', (req, res) => {
+  // look up all the arcticresigination in the mongodb
+  // send the arcticresigination to the Index view as a prop
+  Vinyl.find({}, (error, allVinyls) => {
+    if(allVinyls){
+        res.render('Index', {
+          arcticresigination: allVinyls,
+        })
+    } else {
+        console.log(err)
+    }
+  })
+})
+
+// NEW
+app.get('/arcticresigination/new', (req, res) => {
+  res.render('New')
+})
+// DESTROY
+app.delete('/arcticresigination/:id', (req, res)=>{
+    Vinyl.remove({_id: req.params.id}, (error, deletedVinyl)=>{
+        if (deletedVinyl) {
+            console.log(deletedVinyl)
+        } else {
+            console.log(error)
+        }
+        res.redirect('/arcticresigination')
+    })
+})
+
+//UPDATE
+app.put('/arcticresigination/:id', (req, res) => {
+    Vinyl.findByIdAndUpdate({_id: req.params.id}, {...req.body}, (error, updatedVinyl) => {
+        if (updatedVinyl) {
+            console.log(updatedVinyl)
+        } else {
+            console.log(error)
+        }
+        res.redirect("/vinyls")
+    })
+})
+
+// CREATE
+app.post('/arcticresigination', (req, res) => {
+  // console.log(req.body)
+
+  if (req.body.inStock === 'on') {
+    req.body.inStock = true
+  } else {
+    req.body.inStock = false
+  }
+  Vinyl.create(req.body, (error, createdVinyl) => {
+console.log(error)
+    res.redirect('/arcticresigination')
+  })
+})
+
+//EDIT
+//Can't edit the page as it redirects to itself.
+app.get('/arcticresigination/edit/:id', (req, res) => {
+    Vinyl.findById(req.params._d, (error, vinyl) => {
+        if (vinyl) {
+            console.log(vinyl)
+            res.render('Edit', {
+                vinyl: vinyl
+            })
+        } else {
+            console.log(error)
+        }
+    })
+})
+
+// SHOW
+app.get('/arcticresigination/:id', (req, res) => {
+  Vinyl.findById(req.params.id, (err, foundVinyl) => {
+    res.render('Show', {
+      vinyl: foundVinyl,
+    })
+  })
+})
