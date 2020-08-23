a00870b5fc0c4a350d9cf3f50764d9c5d0f630d2
Philip Gray <pcgrayiii@icloud.com>
2020-08-23 15:44:35 -0700

    commented out the pluralizedViewPath as it was causing path conflicts

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,6 @@
-module.exports = function({Model, ViewPath, Router, booleanKey, pluralizedViewPath}) {
+module.exports = function({Model, ViewPath, Router, booleanKey, /*pluralizedViewPath*/}) {
     //keeps the path plural for the sake of consistency.
-    const plural = pluralizedViewPath || ViewPath.toLowerCase() +'s'
+    //const plural = pluralizedViewPath || ViewPath.toLowerCase() +'s'
 
     //The Routes
     //INDEX
@@ -8,22 +8,22 @@ module.exports = function({Model, ViewPath, Router, booleanKey, pluralizedViewPa
         //finds all models within Model
         Model.find({}, (error, allModels) =>{
             //renders the pathto index
-            res.render(`${plural}/Index`, {
-                [plural]: allModels
+            res.render(`${ViewPath}/Index`, {
+                [ViewPath]: allModels
             })
         })
     })
 
     //NEW
     Router.get('/new', (req, res) => {
-        res.render(`${plural}/New`)
+        res.render(`${ViewPath}/New`)
     })
 
     //DELETE
     Router.delete('/:id', (req, res) => {
         //takes the current model by id and removes it from the collection
         Model.findByIdAndRemove(req.params.id, (error, model) =>{
-            res.redirect(`/${plural}`)
+            res.redirect(`/${ViewPath}`)
         })
     })
 
@@ -39,7 +39,7 @@ module.exports = function({Model, ViewPath, Router, booleanKey, pluralizedViewPa
             req.body,
             { new : true },
             (error, updatedModel) =>{
-                res.redirect(`/${plural}`)
+                res.redirect(`/${ViewPath}`)
             }
         )
     })
@@ -52,7 +52,7 @@ module.exports = function({Model, ViewPath, Router, booleanKey, pluralizedViewPa
         //Creates the Model for t
         Model.create(req.body, (error, createdModel) => {
             //hits up the client once created
-            res.redirect(`/${plural}`)
+            res.redirect(`/${ViewPath}`)
         })
     })
 
@@ -61,7 +61,7 @@ module.exports = function({Model, ViewPath, Router, booleanKey, pluralizedViewPa
         //Finds the document in the collection
         Model.findById(req.params.id, (error, foundModel) => {
             //renders the edit view and passes the found path
-            res.render(`${plural}/Edit`, {
+            res.render(`${ViewPath}/Edit`, {
                 [ViewPath] :foundModel,
             })
         })
@@ -72,7 +72,7 @@ module.exports = function({Model, ViewPath, Router, booleanKey, pluralizedViewPa
         //Find the specific document by id
         Model.findById(req.params.id, (error, foundModel) =>{
             //render the show route and pass it the foundModel
-            res.render(`${plural}/Show`, {
+            res.render(`${ViewPath}/Show`, {
                 [ViewPath]: foundModel,
             })
         })
