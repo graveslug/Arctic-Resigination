71fe56b10f18d5de443c3e09bc22f2622c8ecd82
Philip Gray <pcgrayiii@icloud.com>
2020-08-23 22:44:59 -0700

    cleaned up vinyl.js schema

--- models/vinyl.js
+++ models/vinyl.js
@@ -7,14 +7,36 @@ const mongoose = require('mongoose');
 
 const vinylSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true },
-        album: { type: String, required: true },
-        image: {type: String, required: false},
-        vinylColor: { type: String, required: true },
-        price: {type: Number, required: false},
+        name: {
+            type: String,
+            required: true
+        },
+        album: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: false
+        },
+        vinylColor: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: false
+        },
         inStock: Boolean,
-        currentQuantity: {type: Number, required: true},
-        description: {type: String, required: true},
+        
+        currentQuantity: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
     },
     {
     timestamps: true
