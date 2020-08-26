46472c02b21ca3c3621877358dd3dbad51e21ecd
Philip Gray <pcgrayiii@icloud.com>
2020-08-26 06:42:00 -0700

    error testing

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
     }
     date: {
     type: Date,
-    default: Date.now
+    default: Date.now()
   }
     })
 const User = mongoose.model('User', userSchema)
